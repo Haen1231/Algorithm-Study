106e56b8a09695b1586bcd3a86dd022fac56c532
Haen1231 <dlgodls1231@naver.com>
2022-08-24 22:31:08 +0900

    백준 18353 - 병사 배치하기

---
+++ Baekjoon/baekjoon_18353.py
@@ -0,0 +1,15 @@
+#백준 - 병사 배치하기 / 문제 번호 : 18353 / 문제 분류 : DP / 실버2
+# https://www.acmicpc.net/problem/18353
+
+import sys
+
+n =  int(sys.stdin.readline())
+army = list(map(int,sys.stdin.readline().split()))
+dp = [1]*(n+1)
+
+for i in range(n):
+    for j in range(i):
+        if army[j]>army[i]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(n-max(dp))
