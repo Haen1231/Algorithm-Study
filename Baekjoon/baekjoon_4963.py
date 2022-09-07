b476676014a905b9ed6c4b242d91c552fe7e94c5
Haen1231 <dlgodls1231@naver.com>
2022-09-07 19:48:58 +0900

    백준 4963 - 섬의 개수

---
+++ Baekjoon/baekjoon_4963.py
@@ -0,0 +1,31 @@
+#백준 - 섬의 개수 / 문제번호 : 4963 / 문제 유형 : 그래프 탐색  / 실버 2
+from collections import deque
+import sys
+while True:
+    w, h = map(int, sys.stdin.readline().split())
+    if (w,h) ==(0,0):
+        break
+    land = []
+    visited = [[False]*(w) for _ in range(h)]
+    for _ in range(h):
+        land.append(list(map(int, sys.stdin.readline().split())))
+    
+    cnt = 0
+    direction = [(0,1),(0,-1),(1,0),(-1,0),(-1,-1),(-1,1),(1,-1),(1,1)]
+    #bfs 수행
+    for i in range(h):
+        for j in range(w):
+            if land[i][j] == 1 and not visited[i][j]:
+                cnt += 1
+                queue = deque([(i,j)])
+                visited[i][j] = True
+                while queue:
+                    x, y = queue.popleft()
+                    
+                    for dx,dy in direction:
+                        nx, ny = x+dx,y+dy
+                        if 0 <= nx < h and 0 <= ny < w and land[nx][ny] == 1 and not visited[nx][ny]:
+                                visited[nx][ny] = True
+                                queue.append((nx, ny))
+                          
+    print(cnt)
