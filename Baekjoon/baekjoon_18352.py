1e01609ac4415726a1a4bf1e3902f94b133bfbfd
Haen1231 <ini1231@gmail.com>
2023-04-28 23:28:45 +0900

    백준  - 특정거리의 도시 찾기

--- Baekjoon/baekjoon_18352.py
+++ Baekjoon/baekjoon_18352.py
@@ -1,28 +1,44 @@
+#백준 - 특정 거리의 도시 찾기 / 문제번호 : 18352 / 문제유형 : BFS / 실버2
+#[18352번: 특정 거리의 도시 찾기](https://www.acmicpc.net/problem/18352)
+
 import sys
 from collections import deque
-N,M,K,X = map(int, sys.stdin.readline().split())
-INF = int(1e9)
-gragh = [[INF]*N for _ in range(N)]
-g = [0] *(N+1)
-visited = [False]*N
-queue = [0]*(N+1) 
-for _ in range(K) :
-    a,b = map(int, sys.stdin.readline().split())
-    gragh[a][b] = 1
-    g[a].append(b)
-    
-def bfs(graph, start):
-    queue = deque([start])
-    visited[start] = True
-    cnt = 0
-    while queue:
-        v = queue.popleft()
-        for i in g[v]:
-            if not visited[i]:
-                cnt += 1
-                queue.append(i)
-                visited[i] = True
-                gragh[v][i] = min(gragh[v][i],cnt)
-    return gragh
 
-print(bfs(g,K))
+N,M,K,X = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(N+1)]
+INF = int(1e9)
+
+for _ in range(M):
+    a,b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+
+def bfs(start):
+    visited = [False]*(N+1)
+    distance = [0]*(N+1)
+    visited[start] = True
+    
+    queue = deque([(start,0)])
+    
+    while queue:
+        x, d = queue.popleft()
+        
+        for v in graph[x]:
+            if not visited[v]:
+                visited[v] = True
+                distance[v] = d+1
+                queue.append((v,d+1))
+    return distance
+
+result = bfs(X)
+answer = []
+for i in range(len(result)):
+    if result[i] == K:
+        answer.append(i)
+
+if len(answer) == 0:
+    print(-1)
+else:
+    for a in answer:
+        print(a)
+    
+            
